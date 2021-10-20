61a1a96b972789ccce61bd2bd201cc90f04bc5fa
Ashley <ashleyowenspt@gmail.com>
2021-10-20 10:19:27 -0700

    initializing folder structure

--- project4/restAPI/index.js
+++ project4/restAPI/index.js
@@ -1,4 +1,4 @@
 const router = module.exports = require('express').Router();
 
-router.use('/lodgings', require('./lodgings'));
-router.use('/guests', require('./guests'));
+router.use('/loads', require('./loads'));
+router.use('/boats', require('./boats'));
--- project4/restAPI/server.js
+++ project4/restAPI/server.js
@@ -1,10 +1,9 @@
+const PORT = process.env.PORT || 8080;
 const express = require('express');
 const app = express();
-
+app.set('trust proxy', true);
 app.use('/', require('./index'));
 
-// Listen to the App Engine-specified port, or 8080 otherwise
-const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`);
 });
