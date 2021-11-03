44071b6d79752bf9ea815cec56469a524c3c3651
Ashley <ashleyowenspt@gmail.com>
2021-11-02 19:45:11 -0700

    added client secret credentials file

---
+++ project6/credentials.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "web": {
+        "client_id": "986644349690-0vkcrmk50jmia0021iqtubasn7tchqc7.apps.googleusercontent.com",
+        "project_id": "cs493-oauth2",
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_secret": "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",
+        "redirect_uris": [
+        "http://localhost:8080/oauth",
+        "https://cs493-oauth2.wl.r.appspot.com/oauth"
+        ]
+    }
+}
--- project6/server.js
+++ project6/server.js
@@ -1,8 +1,9 @@
 const PORT = process.env.PORT || 8080;
 const express = require('express');
 const app = express();
+const credentials = require('./credentials.js');
+
 app.set('trust proxy', true);
-// app.use('/', require('./index'));
 app.use(express.static('views'));
 app.use(express.urlencoded({ extended: true }));
 
