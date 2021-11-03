edab564fb2dad4f870f51a19481e9eb603ffb0c5
Ashley <ashleyowenspt@gmail.com>
2021-11-02 20:47:46 -0700

    feat: styled home page

--- project6/server.js
+++ project6/server.js
@@ -1,11 +1,16 @@
 const PORT = process.env.PORT || 8080;
-const express = require('express');
+const express = require("express");
 const app = express();
-const credentials = require('./credentials.js');
+const credentials = require("./credentials.js");
 
-app.set('trust proxy', true);
-app.use(express.static('views'));
+app.set("trust proxy", true);
+app.use(express.static("views"));
 app.use(express.urlencoded({ extended: true }));
+
+app.post("/oauth", async function (req, res) {
+  console.log("send to google");
+  res.status(201).end();
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`);
